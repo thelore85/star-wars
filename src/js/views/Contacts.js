72851c911ed5dcb296cc01f2c3d97e9d1bae7c62
TheLore85 <garofalo.digital@gmail.com>
2023-11-18 14:08:22 +0100

    flux: running with bug v2

--- src/js/views/Contacts.js
+++ src/js/views/Contacts.js
@@ -6,9 +6,10 @@ export default function Contacts() {
 
 	const { store, actions } = useContext(Context);
   
-  const data = store.users;
-  // console.log('testing data: ', Array.isArray(data)); 
-
+  const users = store.users;
+  console.log('data type: ', Array.isArray(users)); 
+  console.log('data: ', users) 
+  
 
   return (
   <section className="d-flex justify-content-center align-items-center bg-secondary" style={{ minHeight: '60vh'}}>
@@ -17,9 +18,9 @@ export default function Contacts() {
       <h2 className="fw-bolder text-white text-center mt-5 " >BEST FRIENDS LIST</h2>
       <div className=" my-5 p-5 rounded bg-white" >
         <ul className="list-unstyled">
-          {data ? data.map(user => 
-          (<li className="border p-3" key={user.id}>{user.name}</li>)):
-          ( <h4 className="text-secondary text-center m-0">No Friends yet? Go out and make some!</h4> )}
+
+          {users.map(user => <li className="border p-3" key={user.id}>{user.name}</li>)}
+          
         </ul>
       </div>
     </div>
